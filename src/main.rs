e0c7593c19ae72359da46b431d2aee26e81d4be6
kert <kaidokert@gmail.com>
2019-06-08 10:28:22 -0700

    Test page faults

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,23 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn handle_panic(_: &PanicInfo) -> ! {
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
     loop {}
 }
 
 #[no_mangle]
-pub extern "C" fn main() {}
+extern "C" fn rust_eh_personality() {}
+
+extern crate heapless;
+use heapless::consts::U8;
+use heapless::Vec;
+
+#[no_mangle]
+pub extern "C" fn main() {
+    let mut xs: Vec<u8, U8> = Vec::new();
+    xs.push(42).unwrap();
+    let val = xs.pop();
+    assert_eq!(val, Some(41));
+}
